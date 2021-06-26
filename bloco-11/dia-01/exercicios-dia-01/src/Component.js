5e08cac472baae32acf43253050a4914367a7039
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-06-26 18:28:28 -0300

    Adicionando um componente de fixaçao

---
+++ bloco-11/dia-01/exercicios-dia-01/src/Component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Este component faz parte da fixação de conteúdo, não é parte dos exercicios.
+
+class Component extends React.Component {
+  render() {
+    return (
+      <p>Olá, meu nome é Lenilsom e eu sou estudante na Trybe desde novembro de 2020. Este é o inicio do bloco de React, ja passei por aqui duas vezes, espero que agora eu consiga progredir de forma satisfatória!</p>
+    )
+  }
+}
+
+export default Component;
